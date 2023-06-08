fdd6271348367c8cbadcef546805c9a19a330d48
Correo Pruebas <correodepruebas2509@gmail.com>
2023-06-08 14:23:50 -0400

    Tipos de datos

---
+++ 03-tiposdedatos.py
@@ -0,0 +1,17 @@
+cadena_texto = 'texto'
+
+print(cadena_texto)
+
+
+numero_entero = 18
+print(numero_entero)
+
+
+numero_float = 20.5
+print(numero_float)
+
+
+variable_boolean = True
+comentario_boolean = 'Booleano tiene que empezar con mayúsculas'
+print(variable_boolean)
+print(comentario_boolean)
